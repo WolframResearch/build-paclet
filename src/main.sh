39f5c9899efa982c393f83a005aae7a89b16cb26
Rick Hennigan <richardh@wolfram.com>
2022-01-28 20:16:48 -0500

    debugging

--- src/main.sh
+++ src/main.sh
@@ -11,3 +11,6 @@ wolframscript ${SCRIPT_DIR}/install_dependencies.wls
 wolframscript ${SCRIPT_DIR}/build_paclet.wls
 
 echo $(cat $GITHUB_ENV)
+echo "pwd="$(pwd)
+echo "BUILD_DIR="$BUILD_DIR
+echo $(ls -la)
