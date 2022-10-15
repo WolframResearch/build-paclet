fcd3ef70b50cf0a3ae1c7975b7ff2f93ad7dc763
Rick Hennigan <richardh@wolfram.com>
2022-10-14 20:19:24 -0400

    Handle wolframscript segmentation faults on exit

--- src/main.sh
+++ src/main.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-set -e
-
 export SCRIPT_DIR=$(dirname ${0})
 
+wolfram_script () { 
+    wolframscript -script "${1}"
+    WS_EXIT_CODE=$(echo $?)
+    case $WS_EXIT_CODE in
+        0)
+            ;;
+        139)
+            echo "::warning::Warning: wolframscript did not exit cleanly"
+            ;;
+        *)
+            exit $WS_EXIT_CODE
+            ;;
+    esac
+}
+
 echo "::group::Installing dependencies..."
-wolframscript ${SCRIPT_DIR}/install_dependencies.wls
+wolfram_script "${SCRIPT_DIR}/install_dependencies.wls"
 echo "::endgroup::"
 
 echo "::group::Building Paclet..."
-wolframscript ${SCRIPT_DIR}/build_paclet.wls
+wolfram_script "${SCRIPT_DIR}/build_paclet.wls"
 echo "::endgroup::"
